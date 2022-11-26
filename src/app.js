741a344550ebc9026f5841ace0b0d1a6a44b35bf
yuliiapanfilova <yuliya.osipchuk@gmail.com>
2022-11-26 14:20:51 +0100

    Added current date and temperature details

---
+++ src/app.js
@@ -0,0 +1,46 @@
+//formating current date and time
+function currentDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+//get current temperature details
+function showTemp(response) {
+  let tempElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  let dateElement = document.querySelector("#date");
+  //let precipitationElement = document.querySelector("#precipitation"); ??not in the API
+  tempElement = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = currentDate(response.data.dt * 1000);
+}
+
+//weather API
+let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&units=metric&appid=${apiKey}`;
+axios.get(apiUrl).then(showTemp);
